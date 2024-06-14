43511a673034670862cd9a8b0ffabc5f8a2786f3
Shuva15 <shuvajitmahanta02@gmail.com>
2024-06-14 23:15:53 +0530

    connected frontend and backend with socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
-const express = require('express')
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:5173",
+    },});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("A user is connected");
+
+  socket.on("message", (message) => {
+    console.log("message fron the frontend" + message);
+  });
+
+  socket.emit("message", "server sends e6");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("server is listning on 3000 port");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from 'react'
+import socket from './socket'
+
 
 function App() {
+
+  useEffect(() => {
+    console.log('useEffect called');
+
+    function handleConnect() {
+      console.log('connected to the server')
+      socket.emit('message', 'frontend sends nf6.');
+    }
+    function handleMessage(message) {
+      console.log('message from server' + message)
+    }
+    socket.on('connect', handleConnect)
+    socket.on('message', handleMessage)
+      
+    console.log('useEffect finished');
+
+      return () => {
+        console.log('clinning up')
+        socket.off('connect');
+        socket.off('message');
+      };
+    
+  }, [])
 
   return (
     <div>Hi</div>
